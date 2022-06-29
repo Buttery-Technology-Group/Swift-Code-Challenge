eedd49ea7adc3ffb1f4588fce8c526d3a094f9fc
Luiz Eduardo <luizdudumr@gmail.com>
2022-06-29 17:39:00 -0300

    User Model and MVVM Architecture base created

---
+++ Swift Code Challenge/UserModel.swift
@@ -0,0 +1,23 @@
+//
+//  UserModel.swift
+//  Swift Code Challenge
+//
+//  Created by Luiz Eduardo Mello dos Reis on 29/06/22.
+//
+
+import Foundation
+
+struct Person: Identifiable{
+    let id: String
+    let firstName: String
+    let lastName: String
+    let photoThumb: String
+    let photo: String
+    let email: String
+    let phonenNumber: String
+    let address: String
+    let lat: String
+    let long: String
+    let city: String
+    let country: String
+}
